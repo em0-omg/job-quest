75e51f26ac30293daea0ec3f68b309c91059d2c5
YOSHIKI KOKUBO <yoshikikokubo@ykmacbook.local>
2019-10-01 21:22:15 +0900

    header added

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,11 @@
     </v-app-bar>
     -->
     <v-content>
-      <!--<HelloWorld/>-->
-      <Nav/>
-      <Home/>
+      <Header/>
+      <!-- <HelloWorld/> -->
+      <!-- <Nav/> -->
+      <!-- <Home/> -->
+      <router-view/>
       <Footer/>
     </v-content>
   </v-app>
@@ -27,16 +29,18 @@
 
 <script>
 //import HelloWorld from './components/HelloWorld';
-import Nav from './components/Nav';
-import Home from './components/Home';
+//import Nav from './components/Nav';
+//import Home from './components/Home';
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
     //HelloWorld,
-    Nav,
-    Home,
+    //Nav,
+    //Home,
+    Header,
     Footer,
   },
   data: () => ({
