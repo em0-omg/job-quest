b45744ed1e03be2d282028c90ecbf38d2d4eca85
小久保 剛基 <yoshiki.kokubo@ted.ntsa.kubota.co.jp>
2019-10-03 18:02:49 +0900

    onSnapshotチャレンジ

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,106 +1,110 @@
 <template>
-    <v-container>
+  <v-container>
+    <v-list three-line>
+      <v-subheader v-text="'Timeline'"></v-subheader>
+      {{ this.postSize }}
+      <template v-for="(item, index) in showPosts">
+        <v-divider :key="index"></v-divider>
 
-        <v-list three-line>
-        <v-subheader
-            v-text="'Timeline'"
-        ></v-subheader>
-        <template v-for="(item, index) in showPosts">
+        <v-list-item :key="item.id" @click="tmp">
+          <v-list-item-avatar>
+            <v-img :src="item.image"></v-img>
+          </v-list-item-avatar>
 
-            <v-divider
-                :key="index"
-            ></v-divider>
-
-            <v-list-item
-                :key="item.id"
-                @click="tmp"
-            >
-                <v-list-item-avatar>
-                    <v-img :src="item.image"></v-img>
-                </v-list-item-avatar>
-
-                <v-list-item-content>
-                    <v-list-item-title v-html="'<h2>'+item.title+'</h2>'"></v-list-item-title>
-                    <v-list-item-content v-html="item.content"></v-list-item-content>
-                    <v-list-item-subtitle v-html="item.createdAt"></v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-        </template>
-        </v-list>
-        <infinite-loading
-            ref="infiniteLoading"
-            @infinite="infiniteHandler"
-        >
-        <div slot="no-results"/>
-        </infinite-loading>
-        <createPostButton/>
-    </v-container>
+          <v-list-item-content>
+            <v-list-item-title v-html="'<h2>'+item.title+'</h2>'"></v-list-item-title>
+            <v-list-item-content v-html="item.content"></v-list-item-content>
+            <v-list-item-subtitle v-html="item.createdAt"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+    <infinite-loading ref="infiniteLoading" @infinite="infiniteHandler">
+      <div slot="no-results" />
+    </infinite-loading>
+    <createPostButton />
+  </v-container>
 </template>
 <script>
-import firebase from 'firebase';
-import createPostButton from './createPostButton';
-import InfiniteLoading from 'vue-infinite-loading';
+import firebase from "firebase";
+import createPostButton from "./createPostButton";
+import InfiniteLoading from "vue-infinite-loading";
 
 export default {
-    name: 'timeline',
-    components: {
-        createPostButton,
-        InfiniteLoading,
+  name: "timeline",
+  components: {
+    createPostButton,
+    InfiniteLoading
+  },
+  data() {
+    return {
+      db: null,
+      posts: [],
+      count: 0,
+      postSize: 0,
+      allPosts: [],
+      showPosts: []
+    };
+  },
+  created: function() {
+    var db = firebase.firestore();
+    var _this = this;
+    _this.db = db;
+  },
+  mounted: function() {
+    var self = this;
+    self.db
+      .collection("users")
+      .doc("company")
+      .collection("posts")
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          self.allPosts.push(doc.data());
+        });
+      });
+    self.db
+      .collection("users")
+      .doc("company")
+      .collection("posts")
+      .get()
+      .then(snap => {
+        self.postSize = snap.size;
+      });
+  },
+  methods: {
+    tmp: function() {
+      console.log("tmp");
     },
-    data() {
-        return {
-            db: null,
-            posts: [],
-            count: 0,
-            postSize: 0,
-            allPosts: [],
-            showPosts: [],
+    infiniteHandler() {
+      var self = this;
+      setTimeout(() => {
+        if (this.count < this.postSize) {
+          var counter = 0;
+          this.allPosts.forEach(function(item) {
+            if (counter >= self.count && counter < self.count + 5) {
+              self.showPosts.push(item);
+            }
+            counter++;
+          });
+          this.count += 5;
+          this.$refs.infiniteLoading.stateChanger.loaded();
+        } else {
+          this.$refs.infiniteLoading.stateChanger.complete();
         }
-    },
-    created: function () {
-        var db = firebase.firestore();
-        var _this = this;
-        _this.db = db;
-
-    },
-    mounted: function () {
-        var self = this;
-        self.db.collection('users').doc('company').collection('posts').get().then( function (querySnapshot) {
-            querySnapshot.forEach( function (doc) {
-                self.allPosts.push(doc.data());
-            })
-        })
-        self.db.collection('users').doc('company').collection('posts').get().then( snap => {
-            self.postSize = snap.size;
-        })
-    },
-    methods: {
-        tmp: function () {
-            console.log('tmp');
-        },
-        infiniteHandler () {
-            var self = this;
-            setTimeout(() => {
-                if (this.count < this.postSize) {
-                    var counter = 0;
-                    this.allPosts.forEach (function (item) {
-                        if (counter >= self.count && counter < self.count+5) {
-                            self.showPosts.push(item);
-                        }
-                        counter++;
-                    })
-                    this.count += 5;
-                    this.$refs.infiniteLoading.stateChanger.loaded();
-                } else {
-                    this.$refs.infiniteLoading.stateChanger.complete();
-                }
-            }, 1000)
-        }
-    },
-    firestore() {
-        return {
-            posts: firebase.firestore().collection('users').doc('company').collection('posts').orderBy('createdAt', 'desc').limit(20)
-        }
+      }, 1000);
     }
-}
+  },
+  firestore() {
+    return {
+      posts: firebase
+        .firestore()
+        .collection("users")
+        .doc("company")
+        .collection("posts")
+        .orderBy("createdAt", "desc")
+        .limit(20)
+    };
+  }
+};
 </script>
